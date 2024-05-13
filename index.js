e609d1982a5b8ed00aa8f40a5d43327daef6331d
RogovinaHelen <rogovina.helen@gmail.com>
2024-05-13 16:18:59 +0400

    lesson4 variables are added with different types

---
+++ index.js
@@ -0,0 +1,26 @@
+let age = 30;
+let name = "Helen";
+const BigInt = 174673491837403746498174049n;
+let isAvailable = false;
+let surname = null;
+let doshirak = undefined;
+let sum = Symbol("Myau");
+
+let passenger = {
+    passengerName: "Pavel",
+    age: 33,
+    country: "Portugal",
+    birthday: "03.04.1991",
+    departureСountry: "Georgia",
+    countryArrival: "Serbia",
+    arrivalTime: "14:30",
+}
+console.log("🚀 ~ age:", age)
+console.log("🚀 ~ name:", name)
+console.log("🚀 ~ BigInt:", BigInt)
+console.log("🚀 ~ isAvailable:", isAvailable)
+console.log("🚀 ~ surname:", surname)
+console.log("🚀 ~ doshirak:", doshirak)
+console.log("🚀 ~ sum:", sum)
+console.log("🚀 ~ passenger:", passenger)
+console.log(age, name, BigInt, isAvailable, surname, doshirak, sum, passenger);
